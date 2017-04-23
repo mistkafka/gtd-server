608fc5e235e50e63844384d1ef9611e00080dca4
mistkafka <mistkafka@gmail.com>
2017-04-24 03:59:10 +0800

    test

--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -11,7 +11,7 @@ const router = express.Router() // eslint-disable-line new-cap
 
 /** GET /health-check - Check service health */
 router.get('/health-check', (req, res) =>
-  res.send('OK')
+  res.send('OK hello-world')
 )
 
 router.use('/users', userRoutes)
